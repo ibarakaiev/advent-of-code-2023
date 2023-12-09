1aba23fe3b55b6ad77d7c85f2a630471c3cdeb99
Igor Barakaiev <ibarakaiev@gmail.com>
2023-12-09 14:13:04 +0100

    Add test for day 9 part 2

--- test/advent_of_code/day_9_test.exs
+++ test/advent_of_code/day_9_test.exs
@@ -3,13 +3,17 @@ defmodule AdventOfCode.Day9Test do
 
   doctest AdventOfCode.Day9
 
-  test "part 1" do
-    input = """
-    0 3 6 9 12 15
-    1 3 6 10 15 21
-    10 13 16 21 30 45
-    """
+  @input """
+  0 3 6 9 12 15
+  1 3 6 10 15 21
+  10 13 16 21 30 45
+  """
 
-    assert AdventOfCode.Day9.solve(input, part: 1) == 114
+  test "part 1" do
+    assert AdventOfCode.Day9.solve(@input, part: 1) == 114
+  end
+
+  test "part 2" do
+    assert AdventOfCode.Day9.solve(@input, part: 2) == 2
   end
 end

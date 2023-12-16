5a5b47ae7f2b34cc07e0828e69a2638ac549ecea
Igor Barakaiev <ibarakaiev@gmail.com>
2023-12-16 14:56:32 +0100

    Comment out real input from tests for Day 16

--- test/advent_of_code/day_16_test.exs
+++ test/advent_of_code/day_16_test.exs
@@ -8,18 +8,18 @@ defmodule AdventOfCode.Day16Test do
     assert AdventOfCode.Day16.solve(input, part: 1) == 46
   end
 
-  test "part 1 (real)" do
-    input = File.read!("test/advent_of_code/inputs/day_16_input.txt")
-    dbg(AdventOfCode.Day16.solve(input, part: 1))
-  end
+  # test "part 1 (real)" do
+  #   input = File.read!("test/advent_of_code/inputs/day_16_input.txt")
+  #   dbg(AdventOfCode.Day16.solve(input, part: 1))
+  # end
 
   test "part 2 (example)" do
     input = File.read!("test/advent_of_code/inputs/day_16_example.txt")
     assert AdventOfCode.Day16.solve(input, part: 2) == 51
   end
 
-  test "part 2 (real)" do
-    input = File.read!("test/advent_of_code/inputs/day_16_input.txt")
-    dbg(AdventOfCode.Day16.solve(input, part: 2))
-  end
+  # test "part 2 (real)" do
+  #   input = File.read!("test/advent_of_code/inputs/day_16_input.txt")
+  #   dbg(AdventOfCode.Day16.solve(input, part: 2))
+  # end
 end

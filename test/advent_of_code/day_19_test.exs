b7c995a1caf47f66ad80d8e22ad6c5a3097c7d82
Igor Barakaiev <ibarakaiev@gmail.com>
2023-12-19 07:50:27 +0100

    Day 19

---
+++ test/advent_of_code/day_19_test.exs
@@ -0,0 +1,34 @@
+defmodule AdventOfCode.Day19est do
+  @moduledoc false
+  use ExUnit.Case
+
+  doctest AdventOfCode.Day19
+
+  @input """
+  px{a<2006:qkq,m>2090:A,rfg}
+  pv{a>1716:R,A}
+  lnx{m>1548:A,A}
+  rfg{s<537:gd,x>2440:R,A}
+  qs{s>3448:A,lnx}
+  qkq{x<1416:A,crn}
+  crn{x>2662:A,R}
+  in{s<1351:px,qqz}
+  qqz{s>2770:qs,m<1801:hdj,R}
+  gd{a>3333:R,R}
+  hdj{m>838:A,pv}
+
+  {x=787,m=2655,a=1222,s=2876}
+  {x=1679,m=44,a=2067,s=496}
+  {x=2036,m=264,a=79,s=2244}
+  {x=2461,m=1339,a=466,s=291}
+  {x=2127,m=1623,a=2188,s=1013}
+  """
+
+  test "part 1" do
+    assert AdventOfCode.Day19.solve(@input, part: 1) == 19_114
+  end
+
+  test "part 2" do
+    assert AdventOfCode.Day19.solve(@input, part: 2) == 167_409_079_868_000
+  end
+end
